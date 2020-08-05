2db5d1c014c31d1f0f0f279e2e33e540c18c6f7b
Ekirkman <edwardk110@gmail.com>
2020-08-05 00:36:23 -0500

    Got it Working

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ export class Satellite {
     type:string;
     operational:boolean;
     launchDate:string;
+    
 
 constructor(name:string, type:string,launchDate:string,orbitType:string,operational:boolean){
     this.name = name;
@@ -12,5 +13,9 @@ constructor(name:string, type:string,launchDate:string,orbitType:string,operatio
     this.orbitType = orbitType;
     this.operational = operational
 }
+shouldShowWarning(){
+    return this.type.toUpperCase() === 'SPACE DEBRIS';
+}
+
 
 }
